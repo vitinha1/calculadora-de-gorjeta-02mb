e65c515702c62149f9c6bb0d1a6ad2c6e2c49f02
Sara Vitor Dias <saravitordias@gmail.com>
2026-05-31 22:03:44 -0300

    quarto commit do js

--- script.js
+++ script.js
@@ -27,19 +27,27 @@ const contaInput = document.querySelector("#conta")
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento){
+function receberPorcentagem(evento){
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
 
-        if(botao.value === evento.target.value){
+        if(botao.value == evento.target.value){
             botao.classList.add("botao-ativo")
         }
     })
 
-    porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== ""){
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else{
+        porcentagem = 0
+    }
 
-   
+
+    console.log("porcentagem")
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
